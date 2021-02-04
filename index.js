cd6e8f028c92e1885170c82e5ffcd4adb34c6eea
pepecoin-contributor <pepecoin-contributor@protonmail.com>
2021-02-04 00:06:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
+const path = require('path');
 
 module.exports = exports = {
-  getRandom:() => {
-    var data = fs.readFileSync('data.json', 'utf8');
+  getRandom: () => {
+    var data = fs.readFileSync(path.join(__dirname, 'data.json'), 'utf8');
     let memes = JSON.parse(data);
     let randIndex = Math.floor(Math.random() * (memes.length - 1));
     let randObj = memes[randIndex];
